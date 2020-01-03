a9aa891d395eb20e118fad791c0cc4bd88c758ef
Rommel Rico <me@rommelrico.com>
2020-01-03 05:36:15 -0800

    :sparkles: Recap: SimpleActor

--- src/main/scala/part1recap/AkkaRecap.scala
+++ src/main/scala/part1recap/AkkaRecap.scala
@@ -1,5 +1,13 @@
 package part1recap
 
+import akka.actor.Actor
+
 object AkkaRecap extends App {
+
+  class SimpleActor extends Actor {
+    override def receive: Receive = {
+      case message => println(s"I received: $message")
+    }
+  }
 
 }

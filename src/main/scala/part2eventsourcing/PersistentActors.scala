f4c968009242e03988528a757df272d82f872526
Rommel Rico <me@rommelrico.com>
2020-01-04 19:43:30 -0800

    :sparkles: EventSourcing: Accountant PersistentActor

--- src/main/scala/part2eventsourcing/PersistentActors.scala
+++ src/main/scala/part2eventsourcing/PersistentActors.scala
@@ -1,9 +1,21 @@
 package part2eventsourcing
 
+import akka.actor.ActorLogging
+import akka.persistence.PersistentActor
+
 object PersistentActors extends App {
 
   /**
    * Scenario: We have a business and an accountant which keeps track of our invoices.
    */
+  class Accountant extends PersistentActor with ActorLogging {
+
+    override def persistenceId: String = ???
+
+    override def receiveCommand: Receive = ???
+
+    override def receiveRecover: Receive = ???
+
+  }
 
 }

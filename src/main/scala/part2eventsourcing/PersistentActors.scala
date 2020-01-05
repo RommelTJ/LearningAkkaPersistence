831472ad06b35c81e537e7bab62f3774cf214b31
Rommel Rico <me@rommelrico.com>
2020-01-04 19:55:50 -0800

    :sparkles: EventSourcing: Handling Invoice messages

--- src/main/scala/part2eventsourcing/PersistentActors.scala
+++ src/main/scala/part2eventsourcing/PersistentActors.scala
@@ -11,15 +11,36 @@ object PersistentActors extends App {
    * Scenario: We have a business and an accountant which keeps track of our invoices.
    */
 
+  // Commands
   case class Invoice(recipient: String, date: Date, amount: Int)
 
+  // Events
+  case class InvoiceRecorded(id: Int, recipient: String, date: Date, amount: Int)
+
+  // Persistent Actor
   class Accountant extends PersistentActor with ActorLogging {
+
+    var latestInvoiceId = 0
+    var totalAmount = 0
 
     // How the events persistent by this Actor will be identified.
     override def persistenceId: String = "simple-accountant" // best practice: Make it unique
 
     // The normal receive method.
-    override def receiveCommand: Receive = ???
+    override def receiveCommand: Receive = {
+      case Invoice(recipient, date, amount) =>
+        // When you receive a command:
+        // 1) You create an EVENT to persist into the store
+        // 2) You persist the event, then pass in a callback that will get triggered once the event is written
+        // 3) You update the Actor's state when the event has persisted.
+        log.info(s"Received invoice for amount: $amount")
+        val event = InvoiceRecorded(latestInvoiceId, recipient, date, amount)
+        persist(event){ e =>
+          latestInvoiceId += 1
+          totalAmount += amount
+          log.info(s"Persisted $e as invoice #${e.id}, for total amount: $totalAmount")
+        }
+    }
 
     // The handler that is called on recovery.
     override def receiveRecover: Receive = ???

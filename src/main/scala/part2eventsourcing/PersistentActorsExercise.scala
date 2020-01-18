f450f2c9d29df9d10af090c7d5cd176de7ed1303
Rommel Rico <me@rommelrico.com>
2020-01-18 07:31:51 -0800

    :sparkles: EventSourcing: Voting Station class

--- src/main/scala/part2eventsourcing/PersistentActorsExercise.scala
+++ src/main/scala/part2eventsourcing/PersistentActorsExercise.scala
@@ -1,5 +1,8 @@
 package part2eventsourcing
 
+import akka.actor.ActorLogging
+import akka.persistence.PersistentActor
+
 object PersistentActorsExercise extends App {
 
   /**
@@ -11,5 +14,20 @@ object PersistentActorsExercise extends App {
    * The actor must be able to recover its state if it's shut down or restarted.
    */
   case class Vote(citizenPID: String, candidate: String)
+
+  class VotingStation extends PersistentActor with ActorLogging {
+
+    override def persistenceId: String = "simple-voting-station"
+
+    override def receiveCommand: Receive = {
+      case Vote(citizenPID, candidate) =>
+        // 1 create the event
+        // 2 persist the event
+        // 3 handle the state change after persisting is successful
+    }
+
+    override def receiveRecover: Receive = ???
+
+  }
 
 }

2bdc357b4fae4aace8bac6060f3fc308fff7a726
Rommel Rico <me@rommelrico.com>
2020-01-19 22:02:30 -0800

    :pencil2: EventSourcing: Diligent Account description

--- src/main/scala/part2eventsourcing/MultiplePersists.scala
+++ src/main/scala/part2eventsourcing/MultiplePersists.scala
@@ -2,4 +2,11 @@ package part2eventsourcing
 
 object MultiplePersists extends App {
 
+  /**
+   * Diligent Account
+   * With every invoice, this actor will persist TWO events.
+   * - a tax record for the fiscal authority
+   * - an invoice record for personal logs or some auditing authority
+   */
+
 }

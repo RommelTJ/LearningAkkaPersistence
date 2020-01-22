8db8862b470c741ef2739e02c2912baaca0ec628
Rommel Rico <me@rommelrico.com>
2020-01-21 20:07:53 -0800

    :sparkles: EventSourcing: Chat class

--- src/main/scala/part2eventsourcing/Snapshots.scala
+++ src/main/scala/part2eventsourcing/Snapshots.scala
@@ -1,5 +1,18 @@
 package part2eventsourcing
 
+import akka.actor.ActorLogging
+import akka.persistence.PersistentActor
+
 object Snapshots extends App {
-  
+
+  class Chat(owner: String, contact: String) extends PersistentActor with ActorLogging {
+
+    override def persistenceId: String = s"$owner-$contact-chat"
+
+    override def receiveCommand: Receive = ???
+
+    override def receiveRecover: Receive = ???
+
+  }
+
 }

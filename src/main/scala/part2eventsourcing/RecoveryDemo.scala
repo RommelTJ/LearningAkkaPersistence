6b769feba0f0580c98a4c76b5739ec5d07be8aba
Rommel Rico <me@rommelrico.com>
2020-01-23 06:21:37 -0800

    :sparkles: EventSourcing: RecoveryActor

--- src/main/scala/part2eventsourcing/RecoveryDemo.scala
+++ src/main/scala/part2eventsourcing/RecoveryDemo.scala
@@ -1,5 +1,18 @@
 package part2eventsourcing
 
+import akka.actor.ActorLogging
+import akka.persistence.PersistentActor
+
 object RecoveryDemo extends App {
-  
+
+  class RecoveryActor extends PersistentActor with ActorLogging {
+    
+    override def persistenceId: String = "recover-actor"
+
+    override def receiveCommand: Receive = ???
+
+    override def receiveRecover: Receive = ???
+
+  }
+
 }

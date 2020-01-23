fc43b834e7814f5128f4087387c53110cea7a1f7
Rommel Rico <me@rommelrico.com>
2020-01-23 06:22:11 -0800

    :sparkles: EventSourcing: Command and Event

--- src/main/scala/part2eventsourcing/RecoveryDemo.scala
+++ src/main/scala/part2eventsourcing/RecoveryDemo.scala
@@ -5,8 +5,11 @@ import akka.persistence.PersistentActor
 
 object RecoveryDemo extends App {
 
+  case class Command(contents: String)
+  case class Event(contents: String)
+
   class RecoveryActor extends PersistentActor with ActorLogging {
-    
+
     override def persistenceId: String = "recover-actor"
 
     override def receiveCommand: Receive = ???

b2706fcfaab0040d893c2d6781ad57607b188a9f
Rommel Rico <me@rommelrico.com>
2020-01-24 07:12:17 -0800

    :sparkles: EventSourcing: CriticalStreamProcessor

--- src/main/scala/part2eventsourcing/PersistAsyncDemo.scala
+++ src/main/scala/part2eventsourcing/PersistAsyncDemo.scala
@@ -1,5 +1,20 @@
 package part2eventsourcing
 
+import akka.actor.ActorLogging
+import akka.persistence.PersistentActor
+
 object PersistAsyncDemo extends App {
-  
+
+  class CriticalStreamProcessor extends PersistentActor with ActorLogging {
+
+    override def persistenceId: String = "critical-stream-processor"
+
+    override def receiveCommand: Receive = ???
+
+    override def receiveRecover: Receive = {
+      case message => log.info(s"Recovered: $message")
+    }
+
+  }
+
 }

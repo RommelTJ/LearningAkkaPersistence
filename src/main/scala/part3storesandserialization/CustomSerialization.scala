6175d479087662a2ea47094deff3f8c06f828456
Rommel Rico <me@rommelrico.com>
2020-03-13 06:58:30 -0700

    :sparkles: Stores: UserRegistered event

--- src/main/scala/part3storesandserialization/CustomSerialization.scala
+++ src/main/scala/part3storesandserialization/CustomSerialization.scala
@@ -3,6 +3,9 @@ package part3storesandserialization
 // command
 case class RegisterUser(email: String, name: String)
 
+// event
+case class UserRegistered(id: Int, email: String, name: String)
+
 object CustomSerialization extends App {
 
 }

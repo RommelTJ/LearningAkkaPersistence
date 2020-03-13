a62e778957dad03c00d265b86ff6d2d7adc25c0b
Rommel Rico <me@rommelrico.com>
2020-03-13 07:04:59 -0700

    :tada: Stores: UserRegistrationSerializer

--- src/main/scala/part3storesandserialization/CustomSerialization.scala
+++ src/main/scala/part3storesandserialization/CustomSerialization.scala
@@ -2,12 +2,25 @@ package part3storesandserialization
 
 import akka.actor.ActorLogging
 import akka.persistence.PersistentActor
+import akka.serialization.Serializer
 
 // command
 case class RegisterUser(email: String, name: String)
 
 // event
 case class UserRegistered(id: Int, email: String, name: String)
+
+// serializer
+class UserRegistrationSerializer extends Serializer {
+  override def identifier: Int = 43272
+
+  override def toBinary(o: AnyRef): Array[Byte] = ???
+
+  // manifest will be None since includeManifest is hardcoded to false
+  override def fromBinary(bytes: Array[Byte], manifest: Option[Class[_]]): AnyRef = ???
+
+  override def includeManifest: Boolean = false
+}
 
 // actor
 class UserRegistrationActor extends PersistentActor with ActorLogging {

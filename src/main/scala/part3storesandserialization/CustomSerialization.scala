44e4290868126c44294be9927dbd3a103f67ec6c
Rommel Rico <me@rommelrico.com>
2020-03-14 04:25:39 -0700

    :sparkles: Stores: Implementing fromBinary method

--- src/main/scala/part3storesandserialization/CustomSerialization.scala
+++ src/main/scala/part3storesandserialization/CustomSerialization.scala
@@ -13,17 +13,29 @@ case class UserRegistered(id: Int, email: String, name: String)
 // serializer
 class UserRegistrationSerializer extends Serializer {
   override def identifier: Int = 43272
+  val SEPARATOR = "//"
 
   override def toBinary(o: AnyRef): Array[Byte] = {
     case event @ UserRegistered(id, email, name) =>
       println(s"Serializing $event")
-      s"[$id//$email//$name]".getBytes
+      s"[$id$SEPARATOR$email$SEPARATOR$name]".getBytes
     case _ =>
       throw new IllegalArgumentException("Only user registration events supported in this serializer")
   }
 
   // manifest will be None since includeManifest is hardcoded to false
-  override def fromBinary(bytes: Array[Byte], manifest: Option[Class[_]]): AnyRef = ???
+  override def fromBinary(bytes: Array[Byte], manifest: Option[Class[_]]): AnyRef = {
+    val string = new String(bytes)
+    val values = string.substring(1, string.length - 1).split(SEPARATOR)
+    val id = values(0).toInt
+    val email = values(1)
+    val name = values(2)
+
+    val result = UserRegistered(id, email, name)
+    println(s"Deserialized $string to $result")
+
+    result
+  }
 
   override def includeManifest: Boolean = false
 }

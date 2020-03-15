983c42270550cd9729f7afc82f2aaa72f259f932
Rommel Rico <me@rommelrico.com>
2020-03-15 09:13:46 -1000

    :sparkles: Advanced: Adding params to AddGuitar

--- src/main/scala/part4practices/EventAdapters.scala
+++ src/main/scala/part4practices/EventAdapters.scala
@@ -3,6 +3,12 @@ package part4practices
 object EventAdapters extends App {
 
   // Store for acoustic guitars
+
+  // Data Structures
   case class Guitar(id: String, model: String, make: String)
-  case class AddGuitar()
+
+  // Command
+  case class AddGuitar(guitar: Guitar, quantity: Int)
+
+  
 }

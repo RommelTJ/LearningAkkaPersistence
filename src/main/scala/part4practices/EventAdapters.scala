454b328deb735c360803017cb7ee143b73ebcbb1
Rommel Rico <me@rommelrico.com>
2020-03-15 09:11:50 -1000

    :sparkles: Advanced: Guitar class

--- src/main/scala/part4practices/EventAdapters.scala
+++ src/main/scala/part4practices/EventAdapters.scala
@@ -3,5 +3,6 @@ package part4practices
 object EventAdapters extends App {
 
   // Store for acoustic guitars
+  case class Guitar(id: String, model: String, make: String)
   case class AddGuitar()
 }

c6ee4ce09436d30ecc7d14a9f7e5a6e84eb54049
Rommel Rico <me@rommelrico.com>
2020-03-16 07:06:21 -1000

    :tada: Advanced: GuitarReadEventAdapter

--- src/main/scala/part4practices/EventAdapters.scala
+++ src/main/scala/part4practices/EventAdapters.scala
@@ -2,7 +2,7 @@ package part4practices
 
 import akka.actor.{ActorLogging, ActorSystem, Props}
 import akka.persistence.PersistentActor
-import akka.persistence.journal.ReadEventAdapter
+import akka.persistence.journal.{EventSeq, ReadEventAdapter}
 import com.typesafe.config.ConfigFactory
 
 import scala.collection.mutable
@@ -39,10 +39,6 @@ object EventAdapters extends App {
     }
 
     override def receiveRecover: Receive = {
-      case event @ GuitarAdded(id, model, make, quantity) =>
-        log.info(s"Recovered $event")
-        val guitar = Guitar(id, model, make, ACOUSTIC)
-        addGuitarInventory(guitar, quantity)
       case event @ GuitarAddedV2(id, model, make, quantity, guitarType) =>
         log.info(s"Recovered $event")
         val guitar = Guitar(id, model, make, guitarType)
@@ -52,6 +48,20 @@ object EventAdapters extends App {
     def addGuitarInventory(guitar: Guitar, quantity: Int): Option[Int] = {
       val existingQuantity = inventory.getOrElse(guitar, 0)
       inventory.put(guitar, existingQuantity + quantity)
+    }
+  }
+
+  class GuitarReadEventAdapter extends ReadEventAdapter {
+    /**
+     * journal -> serializer -> read event adapter -> actor
+     * (bytes)    (GuitarAdded) (GuitarAddedV2)       (receiveRecover)
+     */
+    override def fromJournal(event: Any, manifest: String): EventSeq = {
+      event match {
+        case GuitarAdded(id, model, make, quantity) =>
+          EventSeq.single(GuitarAddedV2(id, model, make, quantity, ACOUSTIC))
+        case other => EventSeq.single(other)
+      }
     }
   }
 

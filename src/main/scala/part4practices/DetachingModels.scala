2a5ad33064487eae3b256c9fc838a781f97f7ab7
Rommel Rico <me@rommelrico.com>
2020-03-17 08:56:25 -1000

    :sparkles: Advanced: Implementing CouponManager Actor

--- src/main/scala/part4practices/DetachingModels.scala
+++ src/main/scala/part4practices/DetachingModels.scala
@@ -3,6 +3,8 @@ package part4practices
 import akka.actor.ActorLogging
 import akka.persistence.PersistentActor
 
+import scala.collection.mutable
+
 object DetachingModels extends App {
 
   case class User(id: String, email: String)
@@ -15,6 +17,25 @@ object DetachingModels extends App {
   case class CouponApplied(code: String, user: User)
 
   class CouponManager extends PersistentActor with ActorLogging {
+    val coupons: mutable.Map[String, User] = new mutable.HashMap[String, User]()
+
+    override def persistenceId: String = "coupon-manager"
+
+    override def receiveCommand: Receive = {
+      case ApplyCoupon(coupon, user) =>
+        if (!coupons.contains(coupon.code)){
+          persist(CouponApplied(coupon.code, user)) { e =>
+            log.info(s"Persisted $e")
+            coupons.put(coupon.code, user)
+          }
+        }
+    }
+
+    override def receiveRecover: Receive = {
+      case event @ CouponApplied(code, user) =>
+        log.info(s"Recovered $event")
+        coupons.put(code, user)
+    }
 
   }
 

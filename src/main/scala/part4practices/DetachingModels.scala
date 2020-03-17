9a837cbbd617d453c81e6a2c39dacb155d5fe5c2
Rommel Rico <me@rommelrico.com>
2020-03-17 08:50:10 -1000

    :sparkles: Advanced: ApplyCoupon

--- src/main/scala/part4practices/DetachingModels.scala
+++ src/main/scala/part4practices/DetachingModels.scala
@@ -8,6 +8,11 @@ object DetachingModels extends App {
   case class User(id: String, email: String)
   case class Coupon(code: String, promotionAmount: Int)
 
+  // Command
+  case class ApplyCoupon(coupon: Coupon, user: User)
+  
+  // Event
+
   class CouponManager extends PersistentActor with ActorLogging {
 
   }

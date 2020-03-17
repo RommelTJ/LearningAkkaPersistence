b68d562b897065f2b12178bfb2370567b3e2c381
Rommel Rico <me@rommelrico.com>
2020-03-17 08:48:41 -1000

    :sparkles: Advanced: User and CouponManager

--- src/main/scala/part4practices/DetachingModels.scala
+++ src/main/scala/part4practices/DetachingModels.scala
@@ -1,5 +1,14 @@
 package part4practices
 
+import akka.actor.ActorLogging
+import akka.persistence.PersistentActor
+
 object DetachingModels extends App {
+
+  case class User(id: String, email: String)
+
+  class CouponManager extends PersistentActor with ActorLogging {
+
+  }
 
 }

1e21e35f1b7fa8bfd37258b4b887a0534ed30e6c
Rommel Rico <me@rommelrico.com>
2020-03-18 06:49:20 -1000

    :sparkles: Advanced: Obtaining read journal

--- src/main/scala/part4practices/PersistenceQueryDemo.scala
+++ src/main/scala/part4practices/PersistenceQueryDemo.scala
@@ -1,10 +1,15 @@
 package part4practices
 
 import akka.actor.ActorSystem
+import akka.persistence.cassandra.query.javadsl.CassandraReadJournal
+import akka.persistence.query.PersistenceQuery
 import com.typesafe.config.ConfigFactory
 
 object PersistenceQueryDemo extends App {
 
   val system = ActorSystem("PersistenceQueryDemo", ConfigFactory.load().getConfig("persistenceQuery"))
+
+  // Read Journal
+  val readJournal = PersistenceQuery(system).readJournalFor[CassandraReadJournal](CassandraReadJournal.Identifier)
 
 }

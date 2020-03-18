fc14315c5d29aa4bab80aa7f02d7482d21465f07
Rommel Rico <me@rommelrico.com>
2020-03-18 06:46:06 -1000

    :sparkles: Advanced: Creating actor system

--- src/main/scala/part4practices/PersistenceQueryDemo.scala
+++ src/main/scala/part4practices/PersistenceQueryDemo.scala
@@ -1,5 +1,10 @@
 package part4practices
 
+import akka.actor.ActorSystem
+import com.typesafe.config.ConfigFactory
+
 object PersistenceQueryDemo extends App {
+
+  val system = ActorSystem("PersistenceQueryDemo", ConfigFactory.load().getConfig("persistenceQuery"))
 
 }

d2883ecf7ffa9829002bc3b21e9ffa8de749de68
Rommel Rico <me@rommelrico.com>
2020-03-18 06:53:13 -1000

    :sparkles: Advanced: More queries

--- src/main/scala/part4practices/PersistenceQueryDemo.scala
+++ src/main/scala/part4practices/PersistenceQueryDemo.scala
@@ -3,6 +3,7 @@ package part4practices
 import akka.actor.ActorSystem
 import akka.persistence.cassandra.query.scaladsl.CassandraReadJournal
 import akka.persistence.query.PersistenceQuery
+import akka.stream.ActorMaterializer
 import com.typesafe.config.ConfigFactory
 
 object PersistenceQueryDemo extends App {
@@ -13,6 +14,8 @@ object PersistenceQueryDemo extends App {
   val readJournal = PersistenceQuery(system).readJournalFor[CassandraReadJournal](CassandraReadJournal.Identifier)
 
   // Persistence Ids query
+  implicit val materializer: ActorMaterializer = ActorMaterializer()(system)
   val persistenceIds = readJournal.persistenceIds()
+  persistenceIds.runForeach(persistenceId => println(s"Found persistence Id: $persistenceId"))
 
 }
